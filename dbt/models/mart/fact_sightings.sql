51de3e1b90cb79eff3402fb6d3a574f636236515
iwle <iwle@icloud.com>
2026-08-16 00:06:15 +0800

    Add fact_sightings to _mart_models.yml

--- dbt/models/mart/fact_sightings.sql
+++ dbt/models/mart/fact_sightings.sql
@@ -10,7 +10,6 @@ with sightings as (
 )
 
 select
-
     sightings.sighting_id as sighting_key,
     {{ from_date_to_char('sightings.date_witness') }} as witness_date_key,
     {{ from_date_to_char('sightings.date_agent') }} as report_date_key,
@@ -20,8 +19,8 @@ select
     dim_location.location_key,
     dim_behavior.behavior_key,
     dim_attribute.attribute_key,
-    (sightings.date_agent - sightings.date_witness)::integer as report_lag_days
-
+    (sightings.date_agent - sightings.date_witness)::integer as report_lag_days,
+    1 as sighting_count
 from sightings
 
 inner join {{ ref('dim_witness') }} as dim_witness
