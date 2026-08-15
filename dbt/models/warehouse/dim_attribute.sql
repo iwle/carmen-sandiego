e6860f3c8996f9b3225eee6acc68a066b27e661f
iwle <iwle@icloud.com>
2026-08-15 23:17:04 +0800

    Add dim_attribute.sql

---
+++ dbt/models/warehouse/dim_attribute.sql
@@ -0,0 +1,46 @@
+{{ config(materialized='table') }}
+
+{# Dimensional model for item attributes or items carried #}
+
+with flags as (
+    select true as flag union all select false
+),
+
+permutations as (
+    select
+        *
+    from (values
+        (true,  true,  true ),
+        (true,  true,  false),
+        (true,  false, true ),
+        (true,  false, false),
+        (false, true,  true ),
+        (false, true,  false),
+        (false, false, true ),
+        (false, false, false)
+    ) as t (has_weapon, has_hat, has_jacket)
+)
+
+select
+    {{ generate_md5_key(['has_weapon', 'has_hat', 'has_jacket']) }} as attribute_key,
+    has_weapon,
+    has_hat,
+    has_jacket,
+    (
+        case when has_weapon then 1 else 0 end
+        + case when has_hat then 1 else 0 end
+        + case when has_jacket then 1 else 0 end
+    ) as attribute_count,
+
+    coalesce(
+        nullif(
+            concat_ws(' + ',
+                case when has_weapon then 'weapon' end,
+                case when has_hat    then 'hat'    end,
+                case when has_jacket then 'jacket' end
+            ),
+            ''
+        ),
+        NULL
+    ) as attribute_text
+from permutations
